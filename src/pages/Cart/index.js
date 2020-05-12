91f74e49d92866a86842264cd6c6daad131ea126
lucas santos <lucas.santos@dedtechsolutions.com.br>
2020-05-12 09:03:11 -0300

    aula 15

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
   MdDelete,
 } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
 
 import { Container, Total, ProductTable } from './styles';
 
-function Cart() {
+function sum(a, b) {
+  return a * b;
+}
+
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -21,35 +27,42 @@ function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img
-                src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-feminino/12/NQQ-0553-012/NQQ-0553-012_zoom2.jpg"
-                alt="Tenis"
-              />
-            </td>
-            <td>
-              <strong>Tenis muito massa</strong>
-              <strong>R$ 120,90</strong>
-            </td>
-            <td>
-              <div>
-                <button type="button">
-                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+          {cart.map((product) => (
+            <tr>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <strong>{formatPrice(product.price)}</strong>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input readOnly value={product.amount} type="number" />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>{formatPrice(sum(product.price * product.amount))}</td>
+              <td>
+                <button
+                  type="button"
+                  onClick={() => {
+                    dispatch({
+                      type: 'DELETE_PRODUCT',
+                      productId: product.id,
+                    });
+                  }}
+                >
+                  <MdDelete size={20} color="#7159c1" />
                 </button>
-                <input readOnly value={2} type="number" />
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="#7159c1" />
-                </button>
-              </div>
-            </td>
-            <td>R$ 241,80</td>
-            <td>
-              <button type="button">
-                <MdDelete size={20} color="#7159c1" />
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </ProductTable>
       <footer>
@@ -63,4 +76,7 @@ function Cart() {
   );
 }
 
-export default Cart;
+const mapStateToProps = (state) => ({
+  cart: state.cart,
+});
+export default connect(mapStateToProps)(Cart);
