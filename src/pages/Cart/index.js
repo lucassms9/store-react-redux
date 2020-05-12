53189ba7ad284dfffa9e9f8abd08309dd95caa04
lucas santos <lucas.santos@dedtechsolutions.com.br>
2020-05-11 23:08:17 -0300

    aula 7

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, Total, ProductTable } from './styles';
 
 function Cart() {
-  return <h1 />;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-feminino/12/NQQ-0553-012/NQQ-0553-012_zoom2.jpg"
+                alt="Tenis"
+              />
+            </td>
+            <td>
+              <strong>Tenis muito massa</strong>
+              <strong>R$ 120,90</strong>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input readOnly value={2} type="number" />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>R$ 241,80</td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar Pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
 
 export default Cart;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,72 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-feminino/12/NQQ-0553-012/NQQ-0553-012_zoom2.jpg"
+          alt="Tenis"
+        />
+
+        <strong>Tesni muito legal</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-feminino/12/NQQ-0553-012/NQQ-0553-012_zoom2.jpg"
+          alt="Tenis"
+        />
+
+        <strong>Tesni muito legal</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-feminino/12/NQQ-0553-012/NQQ-0553-012_zoom2.jpg"
+          alt="Tenis"
+        />
+
+        <strong>Tesni muito legal</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-feminino/12/NQQ-0553-012/NQQ-0553-012_zoom2.jpg"
+          alt="Tenis"
+        />
+
+        <strong>Tesni muito legal</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
 
 export default Home;
